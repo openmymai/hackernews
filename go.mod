12e9e93e53321abbedd5fe88a87542c756ea6474
openmymai <maicmi@me.com>
2024-05-21 14:47:59 +0700

    Hackernews Graphql first commit

---
+++ go.mod
@@ -0,0 +1,29 @@
+module github.com/openmymai/hackernews
+
+go 1.22.2
+
+require (
+	github.com/99designs/gqlgen v0.17.47
+	github.com/docker/docker v24.0.9+incompatible
+	github.com/golang-migrate/migrate v3.5.4+incompatible
+	github.com/golang-migrate/migrate/v4 v4.17.1
+	github.com/vektah/gqlparser/v2 v2.5.12
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+)
---
+++ internal/users/users.go
@@ -0,0 +1,59 @@
+package users
+
+import (
+	"database/sql"
+	_ "database/sql"
+	"log"
+
+	_ "github.com/openmymai/hackernews/internal/pkg/db/mysql"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"name"`
+	Password string `json:"password"`
+}
+
+func (user *User) Create() {
+	statement, err := database.Db.Prepare("INSERT INTO Users(Username, Password) VALUES(?,?)")
+	print(statement)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hashedPassword, err := HashPassword(user.Password)
+	_, err = statement.Exec(user.Username, hashedPassword)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func GetUserIdByUsername(username string) (int, error) {
+	statement, err := database.Db.Prepare("select ID from Users WHERE Username = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	row := statement.QueryRow(username)
+
+	var Id int
+	err = row.Scan(&Id)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			log.Print(err)
+		}
+		return 0, err
+	}
+	return Id, nil
+}
